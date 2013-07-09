edc87a64752046beb7e34fba3401a3f9b9b41ad1
JD Harrington <jd@jdharrington.net>
2013-07-09 18:06:57 -0400

    update specs

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,6 +3,20 @@ require 'spec_helper'
 describe 'se-jenkins::default' do
   let(:chef_run) { ChefSpec::ChefRunner.new(platform: 'ubuntu', version: '12.04').converge('se-jenkins::default') }
 
+  it 'sets the version' do
+    expect(chef_run.node['jenkins']['server']['version']).to match(/\d+\.\d+/)
+  end
+
+  it 'sets the checksum' do
+    expect(chef_run.node['jenkins']['server']['war_checksum']).not_to be_blank
+  end
+
+  %w(git git-client).each do |plugin|
+    it "includes the #{plugin} plugin" do
+      expect(chef_run.node['jenkins']['server']['plugins']).to include(plugin)
+    end
+  end
+
   it 'uses the jenkins::server recipe' do
     expect(chef_run).to include_recipe('jenkins::server')
   end
