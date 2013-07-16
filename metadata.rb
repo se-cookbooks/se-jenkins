c58b20ef232eac4aac05ca8c2a266b81a3a0f5e0
JD Harrington <jd@jdharrington.net>
2013-07-16 19:40:36 -0400

    we need percona and streeteasy_com to build streeteasy

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,8 @@ version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0
 
 depends "git"
 depends "jenkins"
+depends "percona"
 depends "se-ruby", "~> 3.0"
+
+# Projects
+depends "streeteasy_com"
