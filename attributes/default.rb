2e509bfe2d28584208fd9be981895db640cac37f
JD Harrington <jd@jdharrington.net>
2013-07-17 15:25:03 -0400

    Added attributes/default.rb

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['se-jenkins']['campfire']['account']   = ""
+default['se-jenkins']['campfire']['room_id']   = ""
+default['se-jenkins']['campfire']['room_name'] = ""
+default['se-jenkins']['campfire']['token']     = ""
