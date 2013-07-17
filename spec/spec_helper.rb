702c946fb29e48350ae2640621f36e9584f0b5b1
JD Harrington <jd@jdharrington.net>
2013-07-17 18:57:32 -0400

    wipe out vendor/cookbooks between test runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 require 'berkshelf'
 require 'chefspec'
+require 'fileutils'
+
+cookbook_path = File.expand_path(File.dirname(__FILE__) + "/../vendor/cookbooks")
+
+FileUtils.rm_rf(cookbook_path)
 
 Berkshelf.ui.mute do
   berksfile = Berkshelf::Berksfile.from_file('Berksfile')
-  berksfile.install(path: 'vendor/cookbooks')
+  berksfile.install(path: cookbook_path)
 end
