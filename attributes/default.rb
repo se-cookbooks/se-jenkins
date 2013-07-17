583c64716cc3f30dd37560f4c32a5d6a79b3242a
JD Harrington <jd@jdharrington.net>
2013-07-17 15:53:30 -0400

    document attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,9 @@
+# Your Campfire account's subdomain
 default['se-jenkins']['campfire']['account']   = ""
+
+# Your Campfire API token
+default['se-jenkins']['campfire']['token']     = ""
+
+# The numeric id and text name of the room Jenkins should notify
 default['se-jenkins']['campfire']['room_id']   = ""
 default['se-jenkins']['campfire']['room_name'] = ""
-default['se-jenkins']['campfire']['token']     = ""
