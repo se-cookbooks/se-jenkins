ae153ba9e608de82d7e0bf826d09d2daf9f1c749
JD Harrington <jd@jdharrington.net>
2013-07-18 02:57:02 -0400

    Generate depends in metadata.rb from data bag

--- metadata.rb
+++ metadata.rb
@@ -8,8 +8,15 @@ version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0
 
 depends "git"
 depends "jenkins"
-depends "percona"
 depends "se-ruby", "~> 3.0"
 
-# Projects
-depends "streeteasy_com"
+job_config = JSON.parse(File.read('data_bags/jenkins/jobs.json'))
+
+job_config['jobs'].each do |name, config|
+  if config.has_key?('include_recipes')
+    config['include_recipes'].each do |recipe|
+      cookbook = recipe.split('::').first
+      depends cookbook
+    end
+  end
+end
