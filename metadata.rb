01d62641640c4eaf90413228e0579937c6fcfdd5
JD Harrington <jd@jdharrington.net>
2013-08-14 16:59:27 -0400

    Bump to 1.0.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "se-jenkins"
 license          "All rights reserved"
 description      "Installs/Configures se-jenkins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
+version          '1.0.6'
 
 depends "git"
 depends "jenkins"
