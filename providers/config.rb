55de0445dd3eb9d7b6e59c18b5541328dea45f88
JD Harrington <jd@jdharrington.net>
2013-08-15 17:24:23 -0400

    clean up provider

--- providers/config.rb
+++ providers/config.rb
@@ -23,20 +23,3 @@ def store(new_resource)
   end
   new_resource.updated_by_last_action(t.updated_by_last_action?)
 end
-
-#def store
-#  template "#{node['jenkins']['server']['data_dir']}/#{@new_resource.filename}" do
-#    source "#{@new_resouce.filename}.erb"
-#    owner node['jenkins']['server']['user']
-#    group node['jenkins']['server']['group']
-#    mode 0644
-#    notifies :restart, "runit_service[jenkins]"
-#  end
-#end
-
-#alias_method :action_create, :store
-#alias_method :action_update, :store
-
-#def action_delete
-
-#end
