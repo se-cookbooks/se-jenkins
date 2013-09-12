3d6894e8b7875d02af0367a29df494c70e5315c9
JD Harrington <jd@jdharrington.net>
2013-09-12 17:58:33 -0400

    Load chef & aws config from databag

--- recipes/_configure_from_databag.rb
+++ recipes/_configure_from_databag.rb
@@ -1,8 +1,15 @@
+aws      = data_bag_item('aws', 'credentials')
 campfire = data_bag_item('campfire', 'config')
+chef     = data_bag_item('chef', 'credentials')
 github   = data_bag_item('github', 'credentials')
 jenkins  = data_bag_item('jenkins', 'ssh_key')
 
 node.set['se-jenkins']['ssh_key'] = jenkins['private_key']
+
+node.set['se-jenkins']['aws']['access_key_id'] = aws['access_key_id']
+node.set['se-jenkins']['aws']['secret_access_key'] = aws['secret_access_key']
+
+node.set['se-jenkins']['chef']['client_key'] = chef['client_key']
 
 node.set['se-jenkins']['github']['client_id'] = github['client_id']
 node.set['se-jenkins']['github']['client_secret'] = github['client_secret']
